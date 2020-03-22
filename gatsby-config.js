b02db3d81886e110892d1f1cf071d2705757afde
David Dropping <ddropping@gmail.com>
2020-03-22 14:50:14 -0700

    Add graphql playground plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-playground`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
