a7df9eb38fae68dde32ff7b20217f21ae1a2e1d9
David Dropping <ddropping@gmail.com>
2020-03-24 07:54:26 -0700

    Add react bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `ecommerce`,
+    description: `Buy some shirts here`,
     author: `@gatsbyjs`,
   },
   plugins: [
