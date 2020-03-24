db5fe151dedc99f23bc82f5c5231654147711e97
David Dropping <ddropping@gmail.com>
2020-03-24 10:50:43 -0700

    Create constants folder and links files

---
+++ src/constants/social.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebookSquare, FaInstagram, FaTwitterSquare } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebookSquare />,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaInstagram />,
+    url: "https://instagram.com",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com",
+  },
+]
