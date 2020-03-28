69de7a2167798dfa92246e67cb97c9340c416770
David Dropping <ddropping@gmail.com>
2020-03-27 20:00:44 -0700

    Create global state with useReducer for cart

---
+++ src/context/GlobalContextProvider.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export const GlobalStateContext = React.createContext()
+export const GlobalDispatchContext = React.createContext()
+
+const initialState = {
+  cart: [],
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      }
+    default:
+      throw new Error("Bad Action Type")
+  }
+}
+
+const GlobalContextProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return (
+    <GlobalStateContext.Provider value={state}>
+      <GlobalDispatchContext.Provider value={dispatch}>
+        {children}
+      </GlobalDispatchContext.Provider>
+    </GlobalStateContext.Provider>
+  )
+}
+
+export default GlobalContextProvider
