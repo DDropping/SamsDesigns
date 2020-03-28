12b01cc52b8a8c0eb4e9f4fe9c27feff25296aa7
David Dropping <ddropping@gmail.com>
2020-03-28 10:35:33 -0700

    Create cart counter

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { Link } from "gatsby"
 import { GiHamburgerMenu } from "react-icons/gi"
 import { FaShoppingCart } from "react-icons/fa"
 import navLinks from "../../constants/navLinks"
 
 import styles from "./styles/navbar.module.scss"
+import { GlobalStateContext } from "../../context/GlobalContextProvider"
 
 const Navbar = () => {
+  const state = useContext(GlobalStateContext)
   const [isNavDrawer, toggleNavDrawer] = useState(false)
 
   const handleToggleNavDrawer = () => {
     toggleNavDrawer(!isNavDrawer)
   }
+
+  console.log(state.cart.length)
+  var cartItems = state.cart.length
 
   return (
     <div className={styles.container}>
@@ -20,7 +25,12 @@ const Navbar = () => {
         onClick={handleToggleNavDrawer}
       />
       <Link to="/mycart">
-        <FaShoppingCart className={styles.cartIcon} />
+        <div className={styles.cartIcon}>
+          {state.cart.length !== 0 && (
+            <span className={styles.itemCount}>{state.cart.length}</span>
+          )}
+          <FaShoppingCart />
+        </div>
       </Link>
       <div className={styles.logo}>
         <Link to="/" className={styles.logoLink}>
